07554505b4fcbdb19d3efb3fd6aef8e2a40cfe7d
Francois Drolet <francois.drolet@hiloenergie.com>
2024-04-05 14:08:32 -0400

    test env

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use core::convert::TryInto;
-use embedded_svc::mqtt::client::QoS;
+extern crate core;
 
+use core::convert::TryInto;
+use core::time;
+use std::{mem, slice};
+use std::time::Duration;
+use embedded_svc::mqtt::client::QoS;
 use embedded_svc::wifi::{AuthMethod, ClientConfiguration, Configuration};
 
 use esp_idf_svc::hal::prelude::Peripherals;
@@ -9,13 +13,16 @@ use esp_idf_svc::log::EspLogger;
 use esp_idf_svc::timer::EspTaskTimerService;
 use esp_idf_svc::wifi::{AsyncWifi, EspWifi};
 use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition};
+use esp_idf_svc::tls::X509;
 use esp_idf_svc::mqtt::client::{EspMqttClient, MqttClientConfiguration};
 
+
 use log::info;
+use rumqttc::{Transport, TlsConfiguration};
+use rumqttc::v5::{Client, MqttOptions, };
 
 const SSID: &str = env!("WIFI_SSID");
 const PASSWORD: &str = env!("WIFI_PASS");
-const SAS_IOT_HUB: &str = env!("SAS_IOT_HUB");
 
 fn main() -> anyhow::Result<()> {
     esp_idf_svc::sys::link_patches();
@@ -38,16 +45,22 @@ fn main() -> anyhow::Result<()> {
 
     info!("Wifi DHCP info: {:?}", ip_info);
 
-    let mqtt_config = MqttClientConfiguration {
-        client_id: Option::from("test-frank"),
-        username: Option::from("iot-pulse-dev-01.azure-devices.net/test-frank/?api-version=2021-04-12"),
-        password: Option::from(SAS_IOT_HUB),
-        ..Default::default()
-    };
+    let client_cert_bytes: Vec<u8> = include_bytes!("/home/kefran/Documents/certificats/client1-authnID.pem").to_vec();
+    let private_key_bytes: Vec<u8> = include_bytes!("/home/kefran/Documents/certificats/client1-authnID.key").to_vec();
+    let client_cert: X509 = convert_certificate(client_cert_bytes);
+    let private_key: X509 = convert_certificate(private_key_bytes);
+    let transport = Transport::Tls(TlsConfiguration::Simple {
+        ca,
+        alpn: None,
+        client_auth: Some((client_cert, private_key)),
+    });
 
     // Create Client Instance and Define Behaviour on Event
-    let (mut client, _) = EspMqttClient::new("mqtts://iot-pulse-dev-01.azure-devices.net:8883", &mqtt_config)?;
-    client.subscribe("devices/test-frank/messages/devicebound/#", QoS::AtLeastOnce)?;
+    let mut mqttoptions = MqttOptions::new("client1-session-1", "test-frank.canadacentral-1.ts.eventgrid.azure.net", 8883);
+    mqttoptions.set_keep_alive(Duration::from_secs(120));
+    mqttoptions.set_credentials("client1-authnID", "");
+
+    let (mut client, mut connection) = Client::new(mqttoptions, 10);
 
     info!("mqtt client is up");
 
@@ -56,6 +69,23 @@ fn main() -> anyhow::Result<()> {
     std::thread::sleep(core::time::Duration::from_secs(60));
 
     Ok(())
+}
+
+fn convert_certificate(mut certificate_bytes: Vec<u8>) -> X509<'static> {
+    // append NUL
+    certificate_bytes.push(0);
+
+    // convert the certificate
+    let certificate_slice: &[u8] = unsafe {
+        let ptr: *const u8 = certificate_bytes.as_ptr();
+        let len: usize = certificate_bytes.len();
+        mem::forget(certificate_bytes);
+
+        slice::from_raw_parts(ptr, len)
+    };
+
+    // return the certificate file in the correct format
+    X509::pem_until_nul(certificate_slice)
 }
 
 async fn connect_wifi(wifi: &mut AsyncWifi<EspWifi<'static>>) -> anyhow::Result<()> {
